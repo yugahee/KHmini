d4ef4b1f417902d4dc3b6f892b9e5ba5e20e85be
추현정 <chuchu1226@naver.com>
2021-08-02 21:44:51 +0900

    Update Order.java

--- src/view/Order.java
+++ src/view/Order.java
@@ -1,8 +1,23 @@
 package view;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Order {
 	public Order() {
+		JFrame frame = new JFrame();
+		frame.setSize(300,500);
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
 	}
 
+	public static void main(String[] args) {
+		new Order();
+	}
+	
 }

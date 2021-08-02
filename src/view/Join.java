3ee57be837290334207f0ecd8e55fb23fdec9eaf
boming09 <lace23@naver.com>
2021-08-03 01:04:48 +0900

    Join

--- src/view/Join.java
+++ src/view/Join.java
@@ -8,76 +8,97 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 public class Join extends JFrame{
 	public Join() {
-		this.setBackground(Color.WHITE);
+		this.setBounds(500, 100, 300, 500);
 		
-		setLayout(null);
+		// 패널 생성
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.WHITE);
+		panel.setLocation(500, 100);
+		panel.setSize(300, 500);
+		this.add(panel);
+		// 메인 로고
 		ImageIcon mainImage = new ImageIcon("image/mainTitle.jpg");
 		JLabel main = new JLabel(mainImage);
 		main.setLocation(5, 5);
 		main.setSize(40, 60);
-		add(main);
+		panel.add(main);
 		
-		JLabel join = new JLabel("멤버십 가입하고 혜택받기");
+		// Title
+		JLabel join = new JLabel("멤버십 가입하고 혜택 받기");
 		join.setLocation(50, 35);
 		join.setSize(370, 20);
-		join.setFont(new Font("굵음", Font.BOLD, 20));
-		add(join);
+		join.setFont(new Font("굵음", Font.BOLD, 18));
+		panel.add(join);
+		
+		// Sub Title
 		JLabel join2 = new JLabel("회원 정보 입력");
-		join2.setLocation(10, 80);
+		join2.setLocation(10, 75);
 		join2.setSize(280, 20);
-		join2.setFont(new Font("굵음", Font.BOLD, 20));
-		add(join2);
+		join2.setFont(new Font("굵음", Font.BOLD, 15));
+		panel.add(join2);
 		
+		// ID
 		JLabel joinId = new JLabel("아이디 입력헤주세요(이메일)");
-		joinId.setLocation(15, 120);
+		joinId.setLocation(15, 100);
 		joinId.setSize(300, 30);
-		add(joinId);
-		JTextField id = new JTextField(20);
-		id.setLocation(20, 150);
+		panel.add(joinId);
+		JTextField id = new JTextField(20); /*{
+			@Override
+			public void setBorder(Border border) {	// 텍스트 필드 테두리 없애기
+			}
+		};*/
+		id.setLocation(20, 130);
 		id.setSize(250, 30);
-		add(id);
+		panel.add(id);
 		
+		// PassWard
 		JLabel joinPw = new JLabel("비밀번호를 입력해주세요");
-		joinPw.setLocation(15, 200);
+		joinPw.setLocation(15, 180);
 		joinPw.setSize(300, 30);
-		add(joinPw);
+		panel.add(joinPw);
 		JTextField pw = new JTextField(20);
-		pw.setLocation(20, 230);
+		pw.setLocation(20, 210);
 		pw.setSize(250, 30);
-		add(pw);
+		panel.add(pw);
 		
+		// PhoneNumber
 		JLabel joinNum = new JLabel("휴대폰번호를 입력해주세요");
-		joinNum.setLocation(15, 280);
+		joinNum.setLocation(15, 240);
 		joinNum.setSize(300, 30);
-		add(joinNum);
+		panel.add(joinNum);
 		String[] ms = {"SKT", "KT", "LGU+", "알뜰"};
 		JComboBox msList = new JComboBox(ms);
-		msList.setLocation(20, 310);
+		msList.setLocation(20, 270);
 		msList.setSize(60, 30);
-		add(msList);
+		panel.add(msList);
 		JTextField num = new JTextField(20);
-		num.setLocation(90, 310);
+		num.setLocation(90, 270);
 		num.setSize(180, 30);
-		add(num);
+		panel.add(num);
 		
+		// Name
 		JLabel joinName = new JLabel("닉네임을 입력해주세요");
-		joinName.setLocation(15, 360);
+		joinName.setLocation(15, 320);
 		joinName.setSize(300, 30);
-		add(joinName);
+		panel.add(joinName);
 		JTextField name = new JTextField(20);
-		name.setLocation(20, 390);
+		name.setLocation(20, 350);
 		name.setSize(250, 30);
-		add(name);
+		panel.add(name);
 		
+		// Button
 		JButton joinBt = new JButton("완료");
-		joinBt.setBackground(Color.red);
-		joinBt.setLocation(100, 450);
+		joinBt.setBackground(new Color(184, 27, 46));
+		joinBt.setLocation(100, 400);
 		joinBt.setSize(60, 40);
-		add(joinBt);
+		panel.add(joinBt);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
--- src/view/Join_Complete.java
+++ src/view/Join_Complete.java
@@ -8,12 +8,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Join_Complete extends JFrame {
 	public Join_Complete() {
-		this.setBackground(Color.white);
+		this.setBounds(500, 100, 300, 500);
 		
-		setLayout(null);
+		// 패널 생성
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.white);
+		panel.setLayout(null);
 		
 		ImageIcon mainImage = new ImageIcon("Image/mainTitle.jpg");
 		Image img = mainImage.getImage();
@@ -21,27 +25,28 @@ public class Join_Complete extends JFrame {
 		ImageIcon main = new ImageIcon(changeImg);
 		
 		JLabel join = new JLabel(main);
-		join.setLocation(85, 100);
+		join.setLocation(85, 50);
 		join.setSize(120, 230);
-		add(join);
+		panel.add(join);
 		
 		JLabel lb = new JLabel("OO님");
-		lb.setLocation(120, 350);
+		lb.setLocation(120, 300);
 		lb.setSize(70, 20);
 		lb.setFont(new Font("굵음", Font.BOLD, 20));
-		add(lb);
+		panel.add(lb);
 		JLabel lb2 = new JLabel("회원 가입이 완료되었습니다.");
-		lb2.setLocation(20, 370);
+		lb2.setLocation(20, 320);
 		lb2.setSize(400, 20);
 		lb2.setFont(new Font("굵음", Font.BOLD, 20));
-		add(lb2);
+		panel.add(lb2);
 		
 		JButton bt = new JButton("로그인");
-		bt.setBackground(Color.red);
-		bt.setLocation(100, 420);
+		bt.setBackground(new Color(184, 27, 46));
+		bt.setLocation(100, 370);
 		bt.setSize(80, 20);
-		add(bt);
+		panel.add(bt);
 		
+		this.add(panel);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}

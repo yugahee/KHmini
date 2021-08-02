102fbe43ae3ffe272e39650f22a008ad8e8c51ec
추현정 <chuchu1226@naver.com>
2021-08-02 22:07:27 +0900

    Update Order.java

--- src/view/Order.java
+++ src/view/Order.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,17 +10,26 @@ import javax.swing.JPanel;
 public class Order {
 	public Order() {
 		JFrame frame = new JFrame();
-		frame.setSize(300,500);
+		frame.setSize(300, 500);
+		frame.setBackground(Color.WHITE);
+
+		frame.setLayout(null);
+		ImageIcon mainImage = new ImageIcon("image/mainTitle.jpg");
+		JLabel lbmain = new JLabel(mainImage);
+		lbmain.setLocation(5, 5);
+		lbmain.setSize(40, 60);
+		frame.add(lbmain);
+
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel();
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 	}
 
 	public static void main(String[] args) {
 		new Order();
 	}
-	
+
 }
